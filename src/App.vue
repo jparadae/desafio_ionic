46e372c1309e745e542fae0c116b0e0bf1da05e0
jparada <jparada@edutecno.com>
2021-10-16 12:10:30 -0300

    pista de envio notificaciones

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,14 @@ import { FCM } from '@capacitor-community/fcm';
 import { Plugins } from '@capacitor/core';
 const { PushNotifications } = Plugins;
 
+import {
+  Plugins,
+  PushNotification,
+  PushNotificationToken,
+  PushNotificationActionPerformed } from '@capacitor/core';
+
+const { PushNotifications } = Plugins;
+
 export default defineComponent({
   name: 'App',
   components: {
@@ -18,4 +26,37 @@ export default defineComponent({
     IonRouterOutlet
   }
 });
+
+// Register with Apple / Google to receive push via APNS/FCM
+    PushNotifications.register();
+
+    // On success, we should be able to receive notifications
+    PushNotifications.addListener('registration', 
+      (token: PushNotificationToken) => {
+        alert('Push registration success, token: ' + token.value);
+      }
+    );
+
+    // Some issue with our setup and push will not work
+    PushNotifications.addListener('registrationError', 
+      (error: any) => {
+        alert('Error on registration: ' + JSON.stringify(error));
+      }
+    );
+
+    // Show us the notification payload if the app is open on our device
+    PushNotifications.addListener('pushNotificationReceived', 
+      (notification: PushNotification) => {
+        alert('Push received: ' + JSON.stringify(notification));
+      }
+    );
+
+    // Method called when tapping on a notification
+    PushNotifications.addListener('pushNotificationActionPerformed', 
+      (notification: PushNotificationActionPerformed) => {
+        alert('Push action performed: ' + JSON.stringify(notification));
+      }
+    );
+
+
 </script>
